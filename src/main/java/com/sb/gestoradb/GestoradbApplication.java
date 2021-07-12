bc197583a8a86a4abaaf25388448f0dd1d1ab576
vggoncalves <vggoncalves.analista@gmail.com>
2021-07-12 04:58:00 -0300

    start

---
+++ src/main/java/com/sb/gestoradb/GestoradbApplication.java
@@ -0,0 +1,13 @@
+package com.sb.gestoradb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoradbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoradbApplication.class, args);
+	}
+
+}
